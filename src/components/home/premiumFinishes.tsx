c85027c2ee20f9728d67642e3d8a2c0dfd362c1d
Mian Hassan <168227025+customfitboxes@users.noreply.github.com>
2024-05-15 20:48:58 +0500

    done

--- src/components/home/premiumFinishes.tsx
+++ src/components/home/premiumFinishes.tsx
@@ -8,21 +8,21 @@ const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 3,
+    items: 4,
     slidesToSlide: 1,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
+    items: 4,
     slidesToSlide: 1,
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
+    breakpoint: { max: 1024, min: 640 },
     items: 2,
     slidesToSlide: 1,
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 640, min: 0 },
     items: 1,
     slidesToSlide: 1,
   },
@@ -30,7 +30,7 @@ const responsive = {
 
 export const PremiumFinishes = () => {
   return (
-    <Container maxWidth={"lg"}>
+    <Container maxWidth={"xl"}>
       <div className="pb-10 pt-5">
         <h2 className="text-2xl md:text-3xl lg:text-4xl text-center fw_600 leading-tight">
           Premium Finishes
