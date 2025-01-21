a12af9741f8c1750fdbcd8a0393664b28b299edb
Mufaqar <mufaqar@gmail.com>
2025-01-21 12:57:28 +0500

    Menu

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -75,7 +75,7 @@ export const Navbar = (props: any) => {
             className="block text-lg md:hidden"
           >
             {/* <FontAwesomeIcon icon={faBars} /> */}
-            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" className="svg-inline--fa fa-bars " role="img"  viewBox="0 0 448 512"><path fill="currentColor" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"></path></svg>
+            Menu
           </button>
           <SmallNav
             show={showNav}
